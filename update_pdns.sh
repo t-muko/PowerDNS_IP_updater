1fa6834640a0742286ac51a5363fabdc02d14f9c
Teemu Koivisto <teemuko.koivisto@gmail.com>
2024-11-26 15:13:23 +0200

    Capitalise variable names

--- update_pdns.sh
+++ update_pdns.sh
@@ -21,15 +21,15 @@ clean_up () {
 
 usage() {                                 # Function: Print a help message.
   echo
-  echo "Utility to update PowerDNS A or AAAA record for a subdomain."
+  echo "Utility to update PowerDNS A or AAAA record for a PDNS_SUBDOMAIN."
   echo
-  echo "Usage: $0 -s <subdomain> -d <domain> -a <apikey> -u <base_url> -i <IP> [-6]" 
-  echo "  -u base_url: PowerDNS base URL like 'https://pdns.example.com:8081'." 
-  echo "  -a apikey: PowerDNS API key. This should be given as environment variable 'apikey'!"
+  echo "Usage: $0 -s <PDNS_SUBDOMAIN> -d <PDNS_DOMAIN> -a <PDNS_APIKEY> -u <PDNS_BASE_URL> -i <PDNS_HOST_IP> [-6]" 
+  echo "  -u PDNS_BASE_URL: PowerDNS base URL like 'https://pdns.example.com:8081'." 
+  echo "  -a PDNS_APIKEY: PowerDNS API key. This should be given as environment variable 'PDNS_APIKEY'!"
   
-  echo "  -s subdomain: Subdomain to update. Use @ to configure the domain itself." 
-  echo "  -d domain: Domain to update." 
-  echo "  -i IP: IP address to set. If not given, the IP address will be autodetected using https://api64.ipify.org" 
+  echo "  -s PDNS_SUBDOMAIN: PDNS_SUBDOMAIN to update. Use @ to configure the domain itself." 
+  echo "  -d PDNS_DOMAIN: Domain to update." 
+  echo "  -i PDNS_HOST_IP: IP address to set. If not given, the IP address will be autodetected using https://api64.ipify.org" 
   echo "  -c: use current IP address from ipify.org" 
   echo "  -6: Assign AAAA-record for IPv6 instead of IPv4 A-record." 
   echo "  -t TTL: TTL for the record. Default is 600." 
@@ -69,7 +69,7 @@ if [ "$1" == "-h" ]; then
 	exit 0
 fi
 
-[ -z "${apikey}" ] && echo && echo "API key should be passed as an environment variable 'apikey' for security reasons!" && echo
+[ -z "${PDNS_APIKEY}" ] && echo && echo "API key should be passed as an environment variable 'PDNS_APIKEY' for security reasons!" && echo
 [ -z $(which jq) ] && usage && echo -e "\n\n'jq' command line tool is missing! Please install it first.\n" && exit 1
 
 : ${verbose:=0}
@@ -77,71 +77,71 @@ fi
 while getopts "?:h:s:d:a:u:i:6:t:v" flag
 do
 	case "${flag}" in
-		s) subdomain=${OPTARG} ;;
-		d) domain=${OPTARG} ;;
-		a) apikey=${OPTARG} ;;
-		u) base_url=${OPTARG} ;;
-		i) IP=${OPTARG} ;;
+		s) PDNS_SUBDOMAIN=${OPTARG} ;;
+		d) PDNS_DOMAIN=${OPTARG} ;;
+		a) PDNS_APIKEY=${OPTARG} ;;
+		u) PDNS_BASE_URL=${OPTARG} ;;
+		i) PDNS_HOST_IP=${OPTARG} ;;
 		6) use_ipv6=1 ;;
 		t) TTL=${OPTARG} ;;
 		v) verbose=${OPTARG} ;;
 	esac
 done
 
-[ -z "$domain" ] && >&2 echo "Missing '-d domain' flag." && exit_abnormal
-[ -z "$subdomain" ] && >&2 echo "Missing '-s subdomain' flag. Use @ to configure the domain itself." && exit_abnormal
-[ -z "$IP" ] && echo "Missing '-i IP' flag. Autodeting IP address..."
+[ -z "$PDNS_DOMAIN" ] && >&2 echo "Missing '-d PDNS_DOMAIN' flag." && exit_abnormal
+[ -z "$PDNS_SUBDOMAIN" ] && >&2 echo "Missing '-s PDNS_SUBDOMAIN' flag. Use @ to configure the domain itself." && exit_abnormal
+[ -z "$PDNS_HOST_IP" ] && echo "Missing '-i PDNS_HOST_IP' flag. Autodeting IP address..."
 
-[ -z "$base_url" ] && >&2 echo "Missing '-u base_Url' flag. This is PowerDNS base URL like 'https://pdns.example.com:8081'." && exit_abnormal
-[ -z "$apikey" ] && >&2 echo "Missing '-a apikey' flag." && exit_abnormal
+[ -z "$PDNS_BASE_URL" ] && >&2 echo "Missing '-u PDNS_BASE_URL' flag. This is PowerDNS base URL like 'https://pdns.example.com:8081'." && exit_abnormal
+[ -z "$PDNS_APIKEY" ] && >&2 echo "Missing '-a PDNS_APIKEY' flag." && exit_abnormal
 
-if [[ -z "$IP" && $use_ipv6 -eq 0 ]]; then
-	IP=$(curl -s  https://api.ipify.org)
-	echo "Using autodetected IP address: $IP"
+if [[ -z "$PDNS_HOST_IP" && $use_ipv6 -eq 0 ]]; then
+	PDNS_HOST_IP=$(curl -s  https://api.ipify.org)
+	echo "Using autodetected IP address: $PDNS_HOST_IP"
 fi
 
-if [ -z "$IP" ] && [[ $use_ipv6 -ne 0 ]]; then
-	IP=$(curl -s  https://api6.ipify.org)
-	if [ -z "$IP" ]; then
+if [ -z "$PDNS_HOST_IP" ] && [[ $use_ipv6 -ne 0 ]]; then
+	PDNS_HOST_IP=$(curl -s  https://api6.ipify.org)
+	if [ -z "$PDNS_HOST_IP" ]; then
 		echo "Failed to autodetect IPv6 address. Exiting."
 		exit 1
 	fi
-	echo "Using autodetected IPv6 address: $IP"
+	echo "Using autodetected IPv6 address: $PDNS_HOST_IP"
 fi
 
 [[ $use_ipv6 -ne 0 ]] && RRTYPE="AAAA" || RRTYPE="A"
 
 if [[ $RRTYPE == "A" ]] ; then
-  validate_ipv4 $IP
+  validate_ipv4 $PDNS_HOST_IP
 fi
 
 if [[ $RRTYPE == "AAAA" ]] ; then
-  validate_ipv6 $IP
+  validate_ipv6 $PDNS_HOST_IP
 fi
 
 # Make sure domain is period terminated
-if [[ ${domain: -1} != '.' ]]; then
-	domain="${domain}."
+if [[ ${PDNS_DOMAIN: -1} != '.' ]]; then
+	PDNS_DOMAIN="${PDNS_DOMAIN}."
 fi
 
-# Make sure base_url is not terminated with a slash. If it is, remove it.
-if [[ ${base_url: -1} == '/' ]]; then
-	base_url="${base_url::-1}"
+# Make sure PDNS_BASE_URL is not terminated with a slash. If it is, remove it.
+if [[ ${PDNS_BASE_URL: -1} == '/' ]]; then
+	PDNS_BASE_URL="${PDNS_BASE_URL::-1}"
 fi
-# Create endpoint from $base_url
-# e.g. base_url=http://127.0.0.1:8081
-ENDPOINT="$base_url/api/v1/servers/localhost/zones"
+# Create endpoint from $PDNS_BASE_URL
+# e.g. PDNS_BASE_URL=http://127.0.0.1:8081
+ENDPOINT="$PDNS_BASE_URL/api/v1/servers/localhost/zones"
 
-if [[ $subdomain == '@' ]]; then
-	RNAME="$domain"
+if [[ $PDNS_SUBDOMAIN == '@' ]]; then
+	RNAME="$PDNS_DOMAIN"
 else
-	RNAME="$subdomain.$domain"
+	RNAME="$PDNS_SUBDOMAIN.$PDNS_DOMAIN"
 fi
 
 json_message=$(jq -n \
 	--arg RNAME "$RNAME" \
 	--arg RRTYPE "$RRTYPE" \
-	--arg IP "$IP" \
+	--arg IP "$PDNS_HOST_IP" \
 	--arg TTL "$TTL" \
 	'{
 		"rrsets": [
@@ -161,15 +161,15 @@ json_message=$(jq -n \
 	}'
 )
 
-echo -e "Sending request to $ENDPOINT/$domain"
+echo -e "Sending request to $ENDPOINT/$PDNS_DOMAIN"
 echo -e "Payload: $json_message"
 
 set +e 
 
 ## UPDATE
 STATUS=$(curl -Ss \
--X PATCH "$ENDPOINT/$domain" \
--H "X-Api-Key: $apikey" \
+-X PATCH "$ENDPOINT/$PDNS_DOMAIN" \
+-H "X-Api-Key: $PDNS_APIKEY" \
 -H "Content-Type: application/json" \
 -d "$json_message" \
 -w "%{http_code}\n" \
@@ -191,9 +191,9 @@ echo -e "Checking status... \n"
 ## GET status 
 # https://dnssecaccess.euronic.fi/api/v1/servers/localhost/zones/karttahimmeli.fi \
 STATUS=$(curl -L \
-"$ENDPOINT/$domain" \
+"$ENDPOINT/$PDNS_DOMAIN" \
 -H 'Content-Type: application/json' \
--H "X-API-KEY: $apikey" \
+-H "X-API-KEY: $PDNS_APIKEY" \
 -w "%{http_code}\n" \
 -o $DATFILE 2>$ERRFILE)
 
